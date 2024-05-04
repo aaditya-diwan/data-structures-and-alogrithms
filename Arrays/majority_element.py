6c6d6834423c39ec07ffd462d47d314c335bc69a
Aaditya Diwan <aadityapdiwan@gmail.com>
2024-05-04 09:07:53 +0530

    169. Majority Element - LeetCode

---
+++ Arrays/majority_element.py
@@ -0,0 +1,26 @@
+"""
+169. Majority Element
+This algorithm works by initially assuming the first element as the majority candidate. 
+It then iterates through the array, incrementing the count if the current element matches the candidate or decrementing 
+it otherwise. Whenever the count reaches zero, it resets the candidate to the current element and starts counting again. 
+Since the majority element appears more than len(nums)//2 times, 
+this algorithm ensures that the candidate returned at the end is indeed the majority element.
+"""
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
